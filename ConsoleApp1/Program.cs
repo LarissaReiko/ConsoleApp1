493d73c7b4612099a7b4d7ddb5aa9c4c108471cc
Fatec <Fatec@DESKTOP-78MS2FN>
2022-09-20 17:55:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Inicio:
+            Double Num1, Num2;
+            Console.Write("Digite um valor: ");
+            Num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite um valor: ");
+            Num2 = Convert.ToDouble(Console.ReadLine());
+              if (Num1 != Num2) 
+             {
+                if (Num1 < Num2)
+                {
+                    Console.WriteLine("Os números em ordem crescente são: " + Num1 + " e " + Num2);
+                }
+                else 
+                {
+                    Console.WriteLine("Os números em ordem crescente são: " + Num2 + " e " + Num1);
+                }
+               
+            } 
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Os números são iguais, digite novamente");
+                
+                goto Inicio;
+               
+            }
+            
+            Console.ReadKey();
+       }
+        
+    }
+}
